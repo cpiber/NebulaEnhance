20378f6ca98932b993dd35c557a330c7039a68c8
Constantin Piber <cp.piber@gmail.com>
2021-08-29 14:47:26 +0200

    Version bump, lint

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const transform = {
-  '\\.[tj]s$': [ 'rollup-jest', { useCache: false, resolveImports: process.env.RESOLVE, args: { configType: 'tests-internal', silent: true }, configFile: './rollup.config.js' } ],
+  '\\.[tj]s$': [ 'rollup-jest', { useCache: false, args: { configType: 'tests-internal', silent: true }, configFile: './rollup.config.js' } ],
 };
 const moduleNameMapper = {
   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|sass|scss|css)$': '<rootDir>/tests/mocks/fileMock.js',
