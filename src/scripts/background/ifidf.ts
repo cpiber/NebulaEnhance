df0257c3e7fc2ac21ea597a271051e8022af727e
Constantin Piber <cp.piber@gmail.com>
2021-09-16 14:57:24 +0200

    Cleanup

--- src/scripts/background/ifidf.ts
+++ src/scripts/background/ifidf.ts
@@ -22,24 +22,22 @@ export const creatorHasVideo = async <T extends SearchableVideo>(vidcache: Cache
 };
 
 const MIN_PROB = 0.3;
+
+// lowercase, remove accents, split at spaces and sentence marks, remove common words, replace [0-12] with written words
+const exclude = [ 'the', 'is', 'a', 'and', 'or', 'as', 'of', 'be' ];
+const numbers = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve' ];
+const split = (s: string) => normalizeString(s).split(/([\s\-_.,?!:;|]|\p{Pd})+/)
+  .filter(t => t.trim() && exclude.indexOf(t) === -1)
+  .map(v => numbers[+v] || v);
+
 export const matchVideoConfidence = <T extends SearchableVideo>(vidcache: Cache, vids: string | T[], title: string) => {
   if (typeof vids === 'string')
     return vidcache[title] = { confidence: 1, video: vids }; // exact match
   if (!vids.length)
     throw new Error('No videos');
-  // if (vids.length <= 1)
-  //   throw new Error('Not enough data');
-  // lowercase, remove accents, split at spaces and sentence marks, remove common words, replace [0-12] with written words
-  const exclude = [ 'the', 'is', 'a', 'and', 'or', 'as', 'of', 'be' ];
-  const numbers = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve' ];
-  const split = (s: string) => normalizeString(s).split(/([\s\-_.,?!:;|]|\p{Pd})+/)
-    .filter(t => t.trim() && exclude.indexOf(t) === -1)
-    .map(v => numbers[+v] || v);
-  const splitV = (v: T) => split(v.title);
-  // approximate
   // IFIDF - Term frequency, Inverse Document Frequency
   // normalize terms in documents and query
-  const terms = vids.map(splitV);
+  const terms = vids.map(v => split(v.title));
   const query = split(title);
   // for each document, get terms uniquely and count occurences
   const uterms = terms.map(ts => ts.occurence());
@@ -47,9 +45,9 @@ export const matchVideoConfidence = <T extends SearchableVideo>(vidcache: Cache,
   // count and create dict with unique terms
   const { values: dict, occurences: dc } = [ ...uterms.map(t => t.values).flat(), ...uquery.values ].occurence();
   // term frequencies (expand to dict)
-  const ifreq = (n: number, arr: number[], tarr: string[]) => {
+  const ifreq = (n: number, occurences: number[], values: string[]) => {
     let lastind = 0;
-    return dict.map(v => v !== tarr[lastind] ? 0 /* not in doc */ : arr[lastind++] / n /* calculate frequency, goto next */);
+    return dict.map(v => v !== values[lastind] ? 0 /* not in doc */ : occurences[lastind++] / n /* calculate frequency, goto next term */);
   };
   const tf = uterms.map((t, i) => ifreq(terms[i].length, t.occurences, t.values)); // length from original terms (with duplicates)
   const qf = ifreq(query.length, uquery.occurences, uquery.values);
@@ -66,8 +64,6 @@ export const matchVideoConfidence = <T extends SearchableVideo>(vidcache: Cache,
   const tfidf = tf.map(t => t.map((v, index) => v * idf[index]));
   const qfidf = qf.map((v, index) => v * idf[index]);
   const nfidf = norm(qfidf);
-  // sanity check, this should never happen
-  // tfidf.forEach(t => { if (t.length !== qfidf.length) throw new Error("Length mismatch"); });
   // find most similar (cosine similarity maximised)
   const sim = tfidf.map((t, i) => ({ prob: dot(t, qfidf) / (norm(t) * nfidf), vid: i }))
     .sort((a, b) => b.prob - a.prob);
