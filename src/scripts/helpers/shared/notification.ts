7e09d27feb29228b7a09d610c84f6774435bf24d
Constantin Piber <cp.piber@gmail.com>
2022-06-07 11:20:51 +0200

    Fix spamming breaks notification

--- src/scripts/helpers/shared/notification.ts
+++ src/scripts/helpers/shared/notification.ts
@@ -1,9 +1,22 @@
 
+let timeout = 0;
+const stop = () => {
+  window.clearTimeout(timeout);
+  timeout = 0;
+};
+
 export const notification = (text: string | HTMLElement, duration = 1600) => {
+  if (timeout) {
+    stop();
+    document.querySelector('.notification')?.remove();
+  }
   const n = document.querySelector<HTMLDivElement>('.notification') || document.body.appendChild(document.createElement('div'));
   n.classList.add('notification');
-  setTimeout(() => n.classList.add('show'), 1); // to allow transition to run
+  n.classList.remove('show');
   const content = typeof text === 'string' ? document.createTextNode(text) : text;
   n.replaceChildren(n.appendChild(content));
-  setTimeout(() => n.classList.remove('show'), duration);
+  timeout = window.setTimeout(() => {
+    n.classList.add('show');
+    timeout = window.setTimeout(() => (n.classList.remove('show'), stop()), duration);
+  }, 10); // to allow transition to run
 };
