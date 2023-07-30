8430252d9243e9a6d5548d1f1b2679801b05f8d9
Constantin Piber <cp.piber@gmail.com>
2023-07-30 11:41:39 +0200

    [#50] Fix volume indicator for right side compat

--- src/scripts/page/components/volume.ts
+++ src/scripts/page/components/volume.ts
@@ -1,8 +1,10 @@
 import type { Player } from '../player';
 import { findTime } from './htmlhelper';
 
-export const toggleVolumeShow = (player: Player, volumeShow: boolean) =>
+export const toggleVolumeShow = (player: Player, volumeShow: boolean) => {
   player.parentElement.querySelector('.enhancer-volume').classList.toggle('volume-conditional', !volumeShow);
+  player.parentElement.querySelector('#volume-controller, #volume-controller-conditional').id = !volumeShow ? 'volume-controller-conditional' : 'volume-controller';
+};
 
 const attachVolumeText = (player: Player, controls: NodeListOf<Element>, options: { volumeShow: boolean; }) => {
   const volume = player.parentElement.querySelector('#volume-toggle-button').parentElement;
@@ -11,14 +13,17 @@ const attachVolumeText = (player: Player, controls: NodeListOf<Element>, options
   text.className = findTime(controls).className;
   text.classList.toggle('enhancer-hidden', volume.classList.contains('enhancer-hidden'));
   text.classList.add('enhancer-volume');
-  volume.classList.add('enhancer-volume-controller');
-  if (!options.volumeShow)
+  if (!options.volumeShow) {
     text.classList.add('volume-conditional');
+    volume.id = 'volume-controller-conditional';
+  } else {
+    volume.id = 'volume-controller';
+  }
 
   const updateVolume = () => text.textContent = `${(player.volume * 100).toFixed(0)}%`;
   player.addEventListener('volumechange', updateVolume);
   updateVolume();
-  volume.after(text);
+  volume.appendChild(text);
   return text;
 };
 export default attachVolumeText;
