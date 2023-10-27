411175f2cbdec08dd06e1699d4a7630ab40d6efa
Constantin Piber <cp.piber@gmail.com>
2023-10-27 09:22:58 +0200

    Improve notification animation

--- src/scripts/helpers/shared/notification.ts
+++ src/scripts/helpers/shared/notification.ts
@@ -8,15 +8,18 @@ const stop = () => {
 export const notification = (text: string | HTMLElement, duration = 1600, source: HTMLElement = document.body) => {
   if (timeout) {
     stop();
-    document.querySelector('.notification')?.remove();
   }
-  const n = source.querySelector<HTMLDivElement>('.notification') || source.appendChild(document.createElement('div'));
+  const existing = source.querySelector<HTMLDivElement>('.notification');
+  const n = existing || source.appendChild(document.createElement('div'));
   n.classList.add('notification');
-  n.classList.remove('show');
   const content = typeof text === 'string' ? document.createTextNode(text) : text;
   n.replaceChildren(n.appendChild(content));
-  timeout = window.setTimeout(() => {
+
+  const show = () => {
     n.classList.add('show');
     timeout = window.setTimeout(() => (n.classList.remove('show'), stop()), duration);
-  }, 10); // to allow transition to run
+  };
+  // when creating a new element, make sure it first has its position outside the viewport, to play animation
+  if (existing === null) window.setTimeout(show, 1);
+  else show();
 };
