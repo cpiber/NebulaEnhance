648952df818b29f9114e2c05fed5bb3541beed0b
Constantin Piber <cp.piber@gmail.com>
2026-08-23 09:06:42 +0200

    CI: Adjust artifact check

--- .github/verify-artifacts.sh
+++ .github/verify-artifacts.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-dev=$(grep --color=always -n "devClone\|devExport\|(()=>{})" $(find extension-dist -name "*.js"))
+dev=$(grep --color=always -n "devClone\|devExport" $(find extension-dist -name "*.js"))
 if [ -n "$dev" ]; then
   echo "::group::Invalid statements"
   echo "$dev"
   echo "::endgroup::"
   echo "::error ::Found statements that shouldn't be in the production build!"
   exit 1
+fi
+dev=$(grep --color=always -n "(()=>{})" $(find extension-dist -name "*.js"))
+if [ -n "$dev" ]; then
+  echo "::group::Invalid statements"
+  echo "$dev"
+  echo "::endgroup::"
+  echo "::warn ::Found statements that shouldn't be in the production build!"
 fi
 exit 0
